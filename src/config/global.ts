fc7497682a32a6d540eeb7ec112cff343d0cfa4d
Alexander Hunter <icehuntmen@gmail.com>
2022-01-12 15:20:33 +0300

    Changed settings for testing / added prettier

--- src/config/global.ts
+++ src/config/global.ts
@@ -1,14 +1,17 @@
 export default {
-  postgresUrl: process.env.POSTGRES_URL || 'postgres://marketplace:12345@marketplace-postgres:5432/marketplace_db',
-  testingPostgresUrl: 'postgres://test:test@test-postgres:5432/test',
-  listenPort : parseInt(process.env.API_PORT || '5000'),
+  postgresUrl:
+    process.env.POSTGRES_URL ||
+    'postgres://marketplace:12345@marketplace-postgres:5432/marketplace_db',
+  testingPostgresUrl:
+    'postgres://postgresman:test12345@test-postgres:6432/postgres_db',
+  listenPort: parseInt(process.env.API_PORT || '5000'),
   disableSecurity: process.env.DISABLE_SECURITY === 'true',
   dev: {
-    debugMigrations: false
+    debugMigrations: false,
   },
   swagger: {
     title: 'Marketplace api',
     version: '1.0',
-    description: ''
-  }
-}
+    description: '',
+  },
+};
